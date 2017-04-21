8668cc7de59e320e0410c4dd8877f56186f23051
Václav Doležal <fire256@seznam.cz>
2017-04-21 02:00:25 +0200

    Added some doxygen documentation and comments
    Documented CalcLogic.py
    Documented KeyOperations.py

--- src/CalcLogic.py
+++ src/CalcLogic.py
@@ -1,12 +1,37 @@
+##
+# @file CalcLogic.py
+# @author Václav Doležal xdolez76@stud.fit.vutbr.cz
+# @brief Module defining behavior of calculator
+#
+
 from PyQt5.QtWidgets import QWidget
 from KeyEnum import KeyEnum
 import KeyOperations
 
+##
+# @class CalcLogic
+# @brief State and behavior of calculator
+#
 class CalcLogic(object):
+    ##
     number1 = 0
+    ##
     string = ""
+    ##
     pending_op = None
 
+    ##
+    # @brief Evaluates pending binary operation
+    #
+    # Takes `self.number1' as first parameter and `self.string' as second
+    # parameter to function `self.pending_op'.
+    #
+    # If operation is None, converts `self.string' to float.
+    #
+    # @post
+    #   - result is saved to `self.number1'
+    #   - `self.string' is cleared
+    #
     def evaluate(self):
         if self.string:
             if self.pending_op:
@@ -15,19 +40,40 @@ class CalcLogic(object):
                 self.number1 = float(self.string)
             self.string = ""
 
+    ##
+    # @brief Evaluates unary operation
+    #
+    # @param op function representing unary operation
+    # @post
+    #   - result is saved to `self.number1'
+    #   - `self.string' is cleared
+    #
     def evaluate_unary(self, op):
         if op:
             self.number1 = op(self.number1)
         self.string = ""
 
+    ##
+    # @brief Evaluates pending binary operation
+    #
+    # Takes `self.number1' as first parameter and `self.string' as second
+    # parameter to function `self.pending_op'
+    #
+    # @param key button ID
+    # @return string representing result, error message or input string
+    # @post
+    #   - on error resets state
+    #
     def getkey(self, key):
         try:
+            # numerals
             if key < 10:
                 self.string = self.string + str(key)
                 return self.string
             elif KeyEnum(key) == KeyEnum.DOT:
                 if not self.string:
                     self.string = '0.'
+                # ignore any extra dots
                 elif not '.' in self.string:
                     self.string = self.string + '.'
                 return self.string
@@ -35,6 +81,8 @@ class CalcLogic(object):
                 self.evaluate()
                 self.pending_op = KeyOperations.getfn(key)
                 return str(self.number1)
+            # on unary ops, evaluate any pending binary operation, then
+            # perform unary operation on result
             elif KeyOperations.unary(key):
                 self.evaluate()
                 self.evaluate_unary(KeyOperations.getfn(key))
@@ -45,3 +93,5 @@ class CalcLogic(object):
             self.string = ""
             self.number1 = 0
             return 'Error: ' + str(e)
+
+# End of file: CalcLogic.py
--- src/KeyOperations.py
+++ src/KeyOperations.py
@@ -1,12 +1,19 @@
+##
+# @file KeyOperations.py
+# @brief Module mapping buttons to their respective operations
+#
+
 from KeyEnum import KeyEnum
 import mathlib
 
+# dictionary for unary functions
 _unary = {
     KeyEnum.EQUALS: None,
     KeyEnum.FACTORIAL: mathlib.factorial,
     KeyEnum.LOGN: mathlib.logn,
 }
 
+# dictionary for binary functions
 _binary = {
     KeyEnum.PLUS: mathlib.add,
     KeyEnum.MINUS: mathlib.subtract,
@@ -16,6 +23,11 @@ _binary = {
     KeyEnum.ROOT: mathlib.root,
 }
 
+##
+# @brief Searches for function corresponding to `key'
+# @param key button ID
+# @return function reference or None
+#
 def getfn(key):
     op = KeyEnum(key)
     if op in _unary:
@@ -24,8 +36,20 @@ def getfn(key):
         return _binary[op]
     return None
 
+##
+# @brief Test if key represents unary function
+# @param key button ID
+# @return boolean
+#
 def unary(key):
     return KeyEnum(key) in _unary
 
+##
+# @brief Test if key represents binary function
+# @param key button ID
+# @return boolean
+#
 def binary(key):
     return KeyEnum(key) in _binary
+
+# End of file: KeyOperations.py
